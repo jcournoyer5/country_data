6b17ea2eb0d2133a4c58de1810a407bef7e55c40
Jacqueline <jcournoyer5@gmail.com>
2021-02-21 12:52:35 -0500

    simplify code, removing meta-programming

--- country_data/lib/country_data/country.rb
+++ country_data/lib/country_data/country.rb
@@ -1,24 +1,26 @@
 class Country
     attr_accessor :name, :population, :capital, :region, :lat_long, :currencies
-    #reader and writer
+   
     @@all = []
-    #initializing with a hash, iterating through  hash to get the key and the value
-    # metaprogramming
-    # sendinght e
-    # if the key that I'm requesting execute it then saving it to @@all
+  
     def initialize(hash)
-        hash.each do |k,v|
-            # self is an instance => send has as many args as we pass, first one is a method, the rest are the args the method we pass as the first arg as arguments (ask me if you have more questions on this)
-        self.send("#{k}=", v) if self.respond_to?(("#{k}="))
-    end
+        self.name = hash[:name]
+        self.population = hash[:population]
+        self.capital = hash[:capital]
+        self.region = hash[:region],
+        self.lat_long = hash[:lat_long] #{country["latlng"][0]}, #{country["latlng"][1]}",
+        self.currencies = hash[:currencies]
         save
     end
+
     def save
         @@all << self
     end
+
     def self.all
         @@all
-    end    
+    end
+
     # puts out basic data when asked for it, instance method
     def get_country_info
         puts "Name: #{self.name}"
@@ -29,7 +31,7 @@ class Country
     def self.find_by_name(name)
         self.all.find {|country| country.name.downcase == name.downcase}
     end 
-    # isntance method, puts out the "extra" info
+    
     def extra_info 
         puts "Region: #{self.region}"
         puts "Lat & Long: #{self.lat_long}"
